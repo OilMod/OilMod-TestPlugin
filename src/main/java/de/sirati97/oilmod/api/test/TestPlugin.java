c8e0c1bf2e116a6431ff92832b12507922492faa
sirati97 <TheSirati97@gmail.com>
2016-01-22 15:33:33 +0100

    This is the test plugin i used in this video https://youtu.be/ouP23dE0rVI

---
+++ src/main/java/de/sirati97/oilmod/api/test/TestPlugin.java
@@ -0,0 +1,39 @@
+package de.sirati97.oilmod.api.test;
+
+import de.sirati97.oilmod.api.items.ItemRegister;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by sirati97 on 16.01.2016.
+ */
+public class TestPlugin extends JavaPlugin {
+    private ItemRegister itemRegister;
+    private TestItem testItem;
+
+    @Override
+    public void onEnable() {
+        itemRegister = new ItemRegister("oiltst");
+        itemRegister.init();
+        itemRegister.register(testItem = new TestItem());
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (command.getName().equals("oilmodtest") && args.length > 0) {
+                if (args[0].equalsIgnoreCase("testitem")) {
+                    for (int i=0;i<5;i++) {
+                        ItemStack item = testItem.createItemStack(player, 1);
+                        player.getInventory().addItem(item);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
