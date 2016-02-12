52323c7212caf3432219f428a567460f1eb29e97
sirati97 <TheSirati97@gmail.com>
2016-02-12 16:30:15 +0100

    Added Backpack for Inventory API Testing

---
+++ src/main/java/de/sirati97/oilmod/api/test/BackpackItem.java
@@ -0,0 +1,20 @@
+package de.sirati97.oilmod.api.test;
+
+import de.sirati97.oilmod.api.items.NMSItemStack;
+import de.sirati97.oilmod.api.items.OilItemBase;
+import de.sirati97.oilmod.api.items.OilItemStack;
+import org.bukkit.Material;
+
+/**
+ * Created by sirati97 on 12.02.2016.
+ */
+public class BackpackItem extends OilItemBase {
+    public BackpackItem() {
+        super(Material.STICK, 0, 2, 1, "Backpack");
+    }
+
+    @Override
+    public OilItemStack createOilStack(NMSItemStack nmsItemStack) {
+        return new BackpackItemstack(nmsItemStack, this);
+    }
+}
---
+++ src/main/java/de/sirati97/oilmod/api/test/BackpackItemstack.java
@@ -0,0 +1,26 @@
+package de.sirati97.oilmod.api.test;
+
+import de.sirati97.oilmod.api.inventory.InventoryFactoryBase;
+import de.sirati97.oilmod.api.inventory.ModInventoryObject;
+import de.sirati97.oilmod.api.items.NMSItemStack;
+import de.sirati97.oilmod.api.items.OilItemBase;
+import de.sirati97.oilmod.api.items.OilItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+
+/**
+ * Created by sirati97 on 12.02.2016.
+ */
+public class BackpackItemstack extends OilItemStack {
+    ModInventoryObject inventory = InventoryFactoryBase.getInstance().createBasicInventory("inv", this, 9, "Backpack", true);
+
+    public BackpackItemstack(NMSItemStack nmsItemStack, OilItemBase item) {
+        super(nmsItemStack, item);
+    }
+
+    @Override
+    public boolean onUse(Player player, Action action) {
+        player.openInventory(inventory.getBukkitInventory());
+        return true;
+    }
+}
